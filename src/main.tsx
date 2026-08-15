697efe81b978781cac53b62f1197990602fd51e7
Wladimick <61322487+wladimick@users.noreply.github.com>
2026-08-15 13:46:41 -0400

    Load TMDB discovery styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles.css'
 import './data-state.css'
+import './discover.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
